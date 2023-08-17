dd8869a4dacd3ac7532f2f0f0f08020bbe00508e
Arif Shakil <arif35922@outlook.com>
2023-08-17 12:45:33 +0600

    first commit

---
+++ src/main/java/resturant/acl/ResturantAclApplication.java
@@ -0,0 +1,13 @@
+package resturant.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResturantAclApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResturantAclApplication.class, args);
+    }
+
+}
